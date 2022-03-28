ff18977e2430bf29820b50069b8460fcc6eae610
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-03-28 15:07:49 +0530

    Designed data grid and put icons in to navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -18,9 +18,9 @@
         
           <v-navigation-drawer permanent class="cyan darken-4" app>
                 <v-list class=" white--text">
-                  <v-list-item class="px-2" align="center" justify="center" >
+                  <v-list-item class="px-2">
                     <v-list-item-avatar >
-                      <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+                     <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
                     </v-list-item-avatar>
                   </v-list-item>
 
@@ -57,7 +57,26 @@
               </v-list>
             </v-navigation-drawer>
 
-      
+            <v-card-title>
+                <p class="font-weight-bold">
+                  Manage Students
+                </p>  
+                <v-spacer></v-spacer>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+              </v-card-title>
+
+      <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
            
     
    
@@ -69,13 +88,110 @@
     data () {
       return {
         items: [
-          { title: 'charge', icon: 'mdi-ev-plug-type1' },
-          { title: 'profile', icon: 'mdi-account' },
-          { title: 'Session', icon: 'mdi-menu-open' },
-          { title: 'Payments', icon: 'mdi-credit-card-marker' },
-          { title: 'Notifications', icon: 'mdi-bell' },
-          { title: 'Reports', icon: 'mdi-chart-areaspline' },
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Students', icon: 'mdi-school'},
+          { title: 'Course', icon: 'mdi-folder-multiple-outline' },
+          { title: 'Lectures', icon: 'mdi-human-male-board' },
+          { title: 'Library', icon: 'mdi-book-open-page-variant' },
+          { title: 'My Account', icon: 'mdi-account-box-multiple' },
         ],
+
+       headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+
         username:this.$route.params.username
       }
     },
