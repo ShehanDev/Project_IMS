3dc1354b539703914cc2d700e8c66c4ff6593975
Shehan Mallawaarachchi <shehan96mts@gmail.com>
2022-04-13 12:29:03 +0530

    client vue updated

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <template>
-  <div>
-     <h1>Home vue</h1>
- <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet doloribus harum, obcaecati sit itaque animi necessitatibus quam dolor rerum ut distinctio accusantium. Eos doloremque ducimus porro illum facilis obcaecati optio</p>
-  <h1 class="display-4">Massive font </h1>
-  </div>
+  
+    
 
 </template>
 
